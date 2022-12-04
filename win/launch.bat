6f2fa5b692b3c634d67eb73d18cef5b60d67edec
silasfelinus <silasfelinus@gmail.com>
2022-12-03 16:11:18 -0800

    still working on win launcher

--- win/launch.bat
+++ win/launch.bat
@@ -3,26 +3,21 @@
 echo setting parameters
 set INSTALL_DIR="c:"
 set CLONE_DIR="code"
-set VENV_DIR="%INSTALL_DIR%\%CLONE_DIR%\venv"
+set VENV_DIR="%INSTALL_DIR%/\%CLONE_DIR%/\venv"
 set PYTHON_CMD="python3"
 set REQUIREMENTS="requirements.txt"
 set PYTHON="venv\Scripts\python.exe"
 set LAUNCH_SCRIPT="artmaker.py"
 set export ARGS="--precision full --no-half --medvram --listen --port 4242 --administrator --enable-console-prompts --enable-insecure-extension-access"
 
-echo checking Venv
+
 set ERROR_REPORTING=FALSE
-cd %INSTALL_DIR%\%CLONE_DIR%
+cd %INSTALL_DIR%
+cd %CLONE_DIR%
 %PYTHON_CMD% -m venv venv
-if %ERRORLEVEL% == 0 goto :activate_venv
-echo Unable to create venv in directory %VENV_DIR%
-pause
-exit /b
 
-:activate_venv
-echo Venv exists, trying to source...
 
-echo venv %PYTHON%
+venv/Scripts/activate.bat
 if %ERRORLEVEL% == 0 goto :launch
 echo Unable to source venv in directory %VENV_DIR%
 pause
@@ -31,8 +26,6 @@ exit /b
 :launch
 echo launching script
 %PYTHON% %LAUNCH_SCRIPT%
-
-
 pause
 exit /b
 
